06a77c180e1d1e25530414bdc9ea3e3a9ee495c6
Nick Cummings <nickmcummings@gmail.com>
2022-03-09 12:44:16 -0800

    Add timer, background sprites, and movement.

--- Source/main.lua
+++ Source/main.lua
@@ -7,15 +7,61 @@ local gfx <const> = playdate.graphics
 
 local playerSprite = nil
 
+local playerSpeed = 4
+
+local playTimer = nil
+local playTime = 30 * 1000
+
+local function resetTimer()
+  playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
+end
+
 local function initialize()
   local playerImage = gfx.image.new("Images/player")
+  assert(playerImage)
+
   playerSprite = gfx.sprite.new(playerImage)
   playerSprite:moveTo(200, 120)
   playerSprite:add()
+
+  local backgroundImage = gfx.image.new("Images/background2")
+  assert(backgroundImage)
+
+  gfx.sprite.setBackgroundDrawingCallback(
+    function(x, y, width, height)
+      gfx.setClipRect(x, y, width, height)
+      backgroundImage:draw(0, 0)
+      gfx.clearClipRect()
+    end
+  )
+
+  resetTimer()
 end
 
 initialize()
 
 function playdate.update()
+  if playTimer.value == 0 then
+    if playdate.buttonJustPressed(playdate.kButtonA) then
+      resetTimer()
+    end
+  else
+    if playdate.buttonIsPressed(playdate.kButtonUp) then
+      playerSprite:moveBy(0, -playerSpeed)
+    end
+    if playdate.buttonIsPressed(playdate.kButtonRight) then
+      playerSprite:moveBy(playerSpeed, 0)
+    end
+    if playdate.buttonIsPressed(playdate.kButtonDown) then
+      playerSprite:moveBy(0, playerSpeed)
+    end
+    if playdate.buttonIsPressed(playdate.kButtonLeft) then
+      playerSprite:moveBy(-playerSpeed, 0)
+    end
+  end
+
+  playdate.timer.updateTimers()
   gfx.sprite.update()
+
+  gfx.drawText("Time: *" .. math.ceil(playTimer.value/1000) .. "*", 8, 5)
 end
